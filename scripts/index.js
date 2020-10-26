4477e8828a6db023b83ef8624306091ed06cf906
ashenfood <70549343+ashenfood@users.noreply.github.com>
2020-10-26 19:47:33 -0400

    Created tic tac toe lines

--- scripts/index.js
+++ scripts/index.js
@@ -14,8 +14,17 @@ let gameState = [
 function setUpBoard() {
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
-    ctx.moveTo(0, 0);
-    ctx.lineTo(200, 100);
+    ctx.moveTo(100, 0);
+    ctx.lineTo(100, 300);
+    ctx.stroke();
+    ctx.moveTo(200, 0);
+    ctx.lineTo(200, 300);
+    ctx.stroke();
+    ctx.moveTo(0, 100);
+    ctx.lineTo(300, 100);
+    ctx.stroke();
+    ctx.moveTo(0, 200);
+    ctx.lineTo(300, 200);
     ctx.stroke();
 }
 
