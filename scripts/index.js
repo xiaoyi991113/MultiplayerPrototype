9ce7726fd7d2061c9659dbb3a394e201bd95ad98
ashenfood <70549343+ashenfood@users.noreply.github.com>
2020-10-26 19:49:27 -0400

    formatting

--- scripts/index.js
+++ scripts/index.js
@@ -16,13 +16,10 @@ function setUpBoard() {
     var ctx = c.getContext("2d");
     ctx.moveTo(100, 0);
     ctx.lineTo(100, 300);
-    ctx.stroke();
     ctx.moveTo(200, 0);
     ctx.lineTo(200, 300);
-    ctx.stroke();
     ctx.moveTo(0, 100);
     ctx.lineTo(300, 100);
-    ctx.stroke();
     ctx.moveTo(0, 200);
     ctx.lineTo(300, 200);
     ctx.stroke();
