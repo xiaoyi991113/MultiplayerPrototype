b30562a09a88f66f6e94cdc855c045ba5aae7a0f
Alek Kundla <kundla.9@osu.edu>
2020-10-26 19:48:45 -0400

    basic players and locations

--- scripts/index.js
+++ scripts/index.js
@@ -11,12 +11,32 @@ let gameState = [
     [squareStates.BLANK, squareStates.BLANK, squareStates.BLANK],
 ];
 
+let players = {
+    player1: {
+        id: 1,
+        letter: squareStates.X
+    },
+    player2: {
+        id: 2,
+        letter: squareStates.O
+    }
+};
+
+let location = {
+    horizontalPosition: 0,
+    verticalPosition: 0
+};
+
 function setUpBoard() {
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
     ctx.moveTo(0, 0);
     ctx.lineTo(200, 100);
     ctx.stroke();
+}
+
+function markSquare(playerId, location) {
+
 }
 
 window.onload = setUpBoard();
