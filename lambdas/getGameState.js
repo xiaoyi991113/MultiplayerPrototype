7238273122f7cd83904cecd117475554beda2bf8
Alek Kundla <kundla.9@osu.edu>
2021-02-22 20:27:17 -0500

    savign this off since it works

---
+++ lambdas/getGameState.js
@@ -0,0 +1,78 @@
+const https = require('https')
+// Object for the function library that can access other AWS Services
+const AWS = require('aws-sdk');
+
+// Set the region 
+AWS.config.update({region: 'us-east-2'});
+
+// Create the DynamoDB service object
+var ddb = new AWS.DynamoDB({
+    apiVersion: '2012-08-10',
+    httpOptions: {
+    agent: new https.Agent({
+      rejectUnauthorized: true,
+      secureProtocol: "TLSv1_method",
+      ciphers: "ALL"
+    })
+  }
+    
+});
+
+// "main" function
+exports.handler = (event, context, callback) => {
+    
+    console.log('Received event (', event.gameId, '): ', event);
+
+    readState(event.gameId, context, callback);
+};
+
+function readState(gameId, context, callback) {
+    
+    console.log("readState() called");
+    
+    var params = {
+      TableName: 'GameState',
+      Key: {
+        'GameId': {S: 'wMz_lI5VAxJgN8PECT_yFw'}
+      },
+      ProjectionExpression: ''
+    };
+    
+    ddb.getItem(params, 
+        function(err, data) {
+            
+            console.log("Handler called");
+            
+            if (err) {
+                console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+                errorResponse(err.message, context.awsRequestId, callback)
+            }
+            else {
+                console.log("Added item:", JSON.stringify(data, null, 2));
+                callback(null, 
+                    {
+                        statusCode: 201,
+                        body: JSON.stringify(data),
+                        headers: {
+                            'Access-Control-Allow-Origin': '*',
+                        },
+                    }
+                );
+            }
+        }
+    );
+  
+}
+
+function errorResponse(errorMessage, awsRequestId, callback) {
+  callback(null, {
+    statusCode: 500,
+    body: JSON.stringify({
+      Error: errorMessage,
+      Reference: awsRequestId,
+    }),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  });
+}
