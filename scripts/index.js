0f4e301ecd19901b9bd10b791819e4df3739073b
Alek Kundla <kundla.9@osu.edu>
2021-03-01 20:11:52 -0500

    options for https

--- scripts/index.js
+++ scripts/index.js
@@ -378,8 +378,23 @@ function getBoard() {
 
     let gameId = 'HpXQJUPcN3kQU9DFy-vMGg';
 
+    
+    let options = {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer' // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        //body: JSON.stringify(data) // body data type must match "Content-Type" header
+    };
+
     // Call the initial function
-    fetch(boardApiBaseUrl + "/tictactoe" + "/" + gameId)
+    fetch(boardApiBaseUrl + "/tictactoe" + "/" + gameId, options)
         // When we get a response back from the server, convert it to json
         .then((response) => {
             return response.json();
